c2bfe9072078e4e59a1f18d17203898cc45fd1db
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-08-20 23:55:31 +0530

    Conversion of data type

---
+++ 1_Basic/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// TypeConversion
+
+// let score = 33
+// console.log(typeof(score)) // number
+
+let score1 = "33"
+
+
+// let valueInNumber = Number(score1);
+// console.log(typeof(score1));  //String
+// console.log(typeof(valueInNumber)); // number
+
+// what if :- 
+
+// let score2 ="abs33"
+
+// let valueInNumber2 = Number(score2);
+// console.log(typeof(score2));  //String
+// console.log(typeof(valueInNumber2)); // number
+// console.log(valueInNumber2);  // NaN (Not a Number)
+
+// boolean:- 
+
+// let isLogIn = 1;
+// let booleanIsLogIn = boolean(isLogIn);
+// console.log(booleanIsLogIn)
+
+// 1 => true; 0=> false
+//  "" => false
+// "Dipesh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
