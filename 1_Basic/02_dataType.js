4cc1f0d105e1a921a84000289ac5a15b505e7c36
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-08-20 23:24:52 +0530

    variable & dataType

---
+++ 1_Basic/02_dataType.js
@@ -0,0 +1,44 @@
+//  "Use strict" // treat all js as newer version
+
+// alert("Hello");
+// alert(3+3);    // ReferenceError: alert is not defined.  Because we are using node js not web browser.
+
+// console.log("Dipesh"); console.log(3 + 4); // code readenility, we are not use like that
+
+/*
+DataType:- 
+
+A. Primitive dataType
+
+1. number => Represents integers and floating-point numbers. like:- 2^53
+2. bigInt => Represents very large integers. like:- 12345678901234567890n
+3. string => Represents text. like:- "Hello"
+4. boolean => true / false
+5. null => Represents an intentional empty value.Like:- let y = null;
+6. undefined => Variable declared but not assigned a value . like:-  let state
+7. symbol => for defining/ represent  unique identify
+
+B. Non-Primitive (Reference) Data Type
+
+
+1. Object:- Objects store collections of key-value pairs. Arrays, functions, and dates are also objects.
+
+*/
+
+
+// let person = {
+//  name: "Dipesh",
+//  age:20
+// };
+
+// 2. Array:- An array is used to store multiple values in a single variable.
+// let fruits= ["Apple", "Banana", "Graph"];
+
+
+// console.log(typeof (person))
+// console.log(typeof (person.age))
+// console.log(typeof (person.name))
+// console.log(typeof (undefined))    // undefined
+// console.log(typeof (null))        //Object
+
+
