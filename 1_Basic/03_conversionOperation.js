ace12bcc2e77cdd529a6b69f3cc29dec25b8f17c
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-08-21 23:43:05 +0530

    Conversion & Operations

--- 1_Basic/03_conversionOperation.js
+++ 1_Basic/03_conversionOperation.js
@@ -32,4 +32,32 @@ let score1 = "33"
 let someNumber = 33;
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************Operation*************
+
+let value = 3;
+
+let negValue = -value;
+// console.log(negValue);
+
+// let a = 3;
+// let b = 6;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a ** b);
+// console.log(a / b);
+// console.log(a % b);
+
+let str1 = "Dipesh"
+let str2 = " Kumar"
+
+// console.log(str1 + str2); 
+
+// console.log("1" + 2);       // 12
+// console.log(1 + "2");       // 12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "3");    //33
+
+// console.log(1+3-5*2%2);      //4  
