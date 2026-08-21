6928588cc90b8a63500670be3a37febb214d46bd
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-08-21 23:53:51 +0530

    comparision

---
+++ 1_Basic/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log(2>1)     //true
+// console.log(2>=1)       //true
+// console.log(2<1)     //false
+// console.log(2<=1)    //false
+// console.log(2!=1)    //false
+
+// console.log("2">1);      // true
+// console.log("02" > 1);  //true
+
+// (Strictly check) === 
+
+// console.log("2" === 2);     //false  datatype or value dono check hota hai.
+
+// console.log("2" == 2);       //true
