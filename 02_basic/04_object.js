f23a9be0db1b9eab89c134c54409c3726b31f99c
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-08-24 12:48:45 +0530

    Object in depth

---
+++ 02_basic/04_object.js
@@ -0,0 +1,98 @@
+// singleton 
+
+const { log } = require("console")
+
+// Object.create:- we use this method
+
+// const tinderUser = new Object()   // we can create  a object using this mew keyword
+
+const tinderUser = {}               // also using curly brecess
+
+tinderUser.id = "123abs"
+tinderUser.name = "Dipesh"
+tinderUser.isLoggin = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email:"dipesh@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Dipesh",
+            lastname:"kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname)   //Dipesh
+
+// console.log(regularUser.fullname.userfullname.lastname)   //kumar
+
+// *******merge object**********
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj3 = {4:"e", 5:"f"}
+
+const obj7 = {obj1, obj2, obj3}
+                  
+/* 
+
+console.log(obj7)
+{
+  obj1: { '1': 'a', '2': 'b' },
+  obj2: { '3': 'c', '4': 'd' },
+  obj3: { '4': 'e', '5': 'f' }
+}
+*/
+
+
+// ***using Spread:- 90% time we use this method***
+const obj4 = {...obj1, ...obj2, ...obj3}
+// console.log(obj4)                           //{ '1': 'a', '2': 'b', '3': 'c', '4': 'e', '5': 'f' }
+
+//most used syntax when Database se jab value aayegi..
+
+
+// Array ka Object aata  hai 
+
+const users = [
+    {
+        id:1,
+        email:"dipesh@gmial.com"
+    },
+    {
+        id:2,
+        email:"dipesh@gmial.com"
+    },
+    {
+        id:3,
+        email:"dipesh@gmial.com"
+    },
+    {
+        id:4,
+        email:"dipesh@gmial.com"
+    },
+    {
+        id:5,
+        email:"dipesh@gmial.com"
+    },
+]
+
+// console.log(users[1]);    //  { id: 2, email: 'dipesh@gmial.com' }
+
+
+// first we acces array using index, than we use . (dot) to acces object.
+// console.log(users[1].email);   //  dipesh@gmial.com
+
+
+ // put in array :- keys, values, entries
+// console.log(Object.keys(tinderUser))   // [ 'id', 'name', 'isLoggin' ]
+// console.log(Object.values(tinderUser))   // [ '123abs', 'Dipesh', false ]
+
+// console.log(Object.entries(tinderUser))   // [ [ 'id', '123abs' ], [ 'name', 'Dipesh' ], [ 'isLoggin', false ] ]
+
+
+// console.log(tinderUser.hasOwnProperty("isLoggin"))   //true
+
+console.log(tinderUser.hasOwnProperty("isLogged"))     //false
