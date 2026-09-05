d05946519b75c2eee002ac921680559251794cab
Dipesh Kumar <dipeshkumar123mahto@gmail.com>
2026-09-05 11:50:33 +0530

    Scope and there Types

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -1,3 +1,14 @@
+// ### Types of Scope in JavaScript
+
+// 1. Global Scope – Variables accessible from anywhere in the program.
+
+// 2. Function Scope – Variables accessible only inside the function where they are declared.
+
+// 3. Block Scope – Variables declared with `let` or `const` that are accessible only inside the `{ }` block.
+
+// 4. Module Scope – Variables declared inside a JavaScript module that are accessible only within that module.
+
+
 var c = 300
 let a = 200
 
